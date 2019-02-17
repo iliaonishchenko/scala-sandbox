8d8cf85a84ada5793677382cd71be091c7a09e76
Ilia Onishchenko <wopqw@yandex.ru>
2019-02-18 02:30:28 +0300

    some old junk

--- src/main/scala/com/example/example/DriverApp.scala
+++ src/main/scala/com/example/example/DriverApp.scala
@@ -1,5 +1,15 @@
-com.example.example
+package com.example.example
 
 object DriverApp extends App {
   println("Hello world")
+}
+
+object MergeSort {
+  def mergeSort(list: List[Int]): List[Int] = {
+    if (list.length == 1) list
+    else {
+      val n = list.length / 2
+      val (head, tail) = list.splitAt(n)
+    }
+  }
 }
